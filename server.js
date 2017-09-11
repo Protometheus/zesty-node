d4b12c02b2a3e2e102097d762815c1f31ca7c2cb
Mohammad Oweis <hamada.oweis@gmail.com>
2017-09-11 17:28:26 -0400

    listening

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
 app.get('/', function(req, res) {
   res.send('Hello, everybody!\n');
 });
@@ -11,3 +9,5 @@ app.get('/code/challenge', function(req, res) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.send('Welcome to the challenge!\n');
 })
+
+app.listen(process.env.PORT || 5000)
