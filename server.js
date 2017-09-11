fdd809e1e55531dcb8d0216b1943a6677407000a
Mohammad Oweis <hamada.oweis@gmail.com>
2017-09-11 17:22:44 -0400

    changed port

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res) {
   res.send('Hello, everybody!\n');
 });
 
 app.get('/code/challenge', function(req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
 	res.send('Welcome to the challenge!\n');
 })
-
-app.listen(8080);
-console.log('Listening on port 8080...');
