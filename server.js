33655f8e38b2381ea923765d3652741a748def78
Mohammad Oweis <hamada.oweis@gmail.com>
2017-09-11 17:42:36 -0400

    added json

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello, everybody!\n');
+	res.redirect('/code/challenge')
 });
 
 app.get('/code/challenge', function(req, res) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.send('Welcome to the challenge!\n');
+	res.header("Content-Type", "application/json; charset=utf-8")
+	res.status(201)
+	
+	res.json({
+		"email": "oweismo.applicant@gmail.com",
+		"website": "https://github.com/Protometheus/zesty-node",
+		"name": {
+			"first": "Mohammad",
+			"last": "Oweis"
+		}});
 })
 
 app.listen(process.env.PORT || 5000)
